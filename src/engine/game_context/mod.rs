661d4b91c7eca0ff5d68db81f245c56a573d6a72
ToothlessBrush <73563930+ToothlessBrush@users.noreply.github.com>
2024-12-30 00:18:45 -0600

    started documentation

--- src/engine/game_context/mod.rs
+++ src/engine/game_context/mod.rs
@@ -11,17 +11,31 @@ use fps_manager::FPSManager;
 use input_manager::InputManager;
 use node_manager::NodeManager;
 
+/// The main game context, containing all the necessary information for the game to run.
+/// This includes the window, the nodes, the frame manager, the input manager, and the shadow distance.
 pub struct GameContext {
+    /// The window of the game.
     pub window: glfw::PWindow,
-
+    /// The node manager of the game.
     pub nodes: NodeManager,
+    /// The frame manager of the game.
     pub frame: FPSManager,
+    /// The input manager of the game.
     pub input: InputManager,
-
+    /// The shadow distance of the game.
     pub shadow_distance: f32,
 }
 
 impl GameContext {
+    /// Creates a new game context with the given events, glfw, and window.
+    ///
+    /// # Arguments
+    /// - `events` - The input events of the game.
+    /// - `glfw` - The glfw context of the game.
+    /// - `window` - The window of the game.
+    ///
+    /// # Returns
+    /// The new game context.
     pub fn new(
         events: GlfwReceiver<(f64, glfw::WindowEvent)>,
         glfw: glfw::Glfw,
@@ -37,6 +51,10 @@ impl GameContext {
         }
     }
 
+    /// lock the cursor inside the window.
+    ///
+    /// # Arguments
+    /// - `lock` - Whether to lock the cursor or not.
     pub fn lock_cursor(&mut self, lock: bool) {
         if lock {
             self.window.set_cursor_mode(glfw::CursorMode::Disabled);
