9220f54c014854d34234a1a7b1fd2117bfed0a79
John Klein <johnklein514@gmail.com>
2026-07-15 01:10:17 -0500

    audio improvements

--- crates/maple_audio/src/plugin.rs
+++ crates/maple_audio/src/plugin.rs
@@ -1,8 +1,7 @@
-use std::path::PathBuf;
-
 use glam::{Quat, Vec3};
-use kira::{AudioManagerSettings, DefaultBackend, Tween, listener, track::SpatialTrackBuilder};
+use kira::{AudioManagerSettings, DefaultBackend, Tween, track::SpatialTrackBuilder};
 use maple_app::Plugin;
+use maple_engine::prelude::Frame;
 
 use crate::{
     asset::{AudioData, AudioLoader},
@@ -24,35 +23,61 @@ impl Plugin for AudioPlugin {
     fn update(&self, app: &mut maple_app::App<maple_app::Running>) {
         let mut manager = app.context().get_resource_mut::<AudioManager>();
 
+        for (audio, settings) in std::mem::take(&mut manager.queue) {
+            let Some(data) = app.context().assets.get(&audio) else {
+                manager.queue.push_back((audio, settings));
+                continue;
+            };
+            match &data.data {
+                AudioData::Static(sound_data) => {
+                    let _handle = manager
+                        .manager
+                        .play(sound_data.clone().with_settings(settings.into()));
+                }
+                AudioData::Streaming { .. } => continue,
+            }
+        }
+
         let listeners = app.context().scene.collect::<AudioListener>();
 
         let Some(active_listener) = listeners
             .iter()
             .max_by_key(|listener| listener.read().priority)
         else {
+            manager.listener = None;
             return;
         };
 
-        manager.listener.set_position(
+        if manager.listener.is_none() {
+            let id = manager
+                .manager
+                .add_listener(Vec3::default(), Quat::default())
+                .expect("listener to be created there should only be one listener");
+            manager.listener = Some(id);
+        }
+        let listener = manager.listener.as_mut().unwrap();
+
+        let tween = Tween {
+            duration: app.context().get_resource::<Frame>().time_delta,
+            ..Default::default()
+        };
+
+        listener.set_position(
             active_listener.read().transform.world_space().position,
-            Tween::default(),
+            tween,
+        );
+        listener.set_orientation(
+            active_listener.read().transform.world_space().rotation,
+            tween,
         );
 
-        manager.listener.set_orientation(
-            active_listener.read().transform.world_space().rotation,
-            Tween::default(),
-        );
+        let id = listener.id();
 
         app.context().scene.for_each::<AudioSource>(&mut |source| {
             let handle = source.handle.get_or_insert_with(|| {
-                let listener_id = manager.listener.id();
                 manager
                     .manager
-                    .add_spatial_sub_track(
-                        listener_id,
-                        Vec3::default(),
-                        SpatialTrackBuilder::default(),
-                    )
+                    .add_spatial_sub_track(id, Vec3::default(), SpatialTrackBuilder::default())
                     .unwrap()
             });
 
@@ -60,7 +85,7 @@ impl Plugin for AudioPlugin {
 
             for (audio, settings) in std::mem::take(&mut source.queue) {
                 let Some(data) = app.context().assets.get(&audio) else {
-                    source.queue.push_back((audio, settings)); // not loaded yet, retry next frame
+                    source.queue.push_back((audio, settings)); // not loaded
                     continue;
                 };
                 match &data.data {
@@ -68,7 +93,7 @@ impl Plugin for AudioPlugin {
                         let _handle =
                             handle.play(sound_data.clone().with_settings(settings.into()));
                     }
-                    AudioData::Streaming { .. } => continue, // handle separately, see earlier note
+                    AudioData::Streaming { .. } => continue, // TODO
                 }
             }
         })
--- crates/maple_audio/src/resource.rs
+++ crates/maple_audio/src/resource.rs
@@ -1,18 +1,28 @@
+use std::collections::VecDeque;
+
 use glam::{Quat, Vec3};
 use kira::{AudioManager as Manager, listener::ListenerHandle};
-use maple_engine::prelude::Resource;
+use maple_engine::{asset::AssetHandle, prelude::Resource};
+
+use crate::{asset::Audio, nodes::audio_source::SoundSettings};
 
 pub struct AudioManager {
     pub(crate) manager: Manager,
-    pub(crate) listener: ListenerHandle,
+    pub(crate) listener: Option<ListenerHandle>,
+    pub(crate) queue: VecDeque<(AssetHandle<Audio>, SoundSettings)>,
 }
 
 impl AudioManager {
-    pub(crate) fn new(mut manager: Manager) -> Self {
-        let listener = manager
-            .add_listener(Vec3::default(), Quat::default())
-            .expect("listener to be created there should only be one listener");
-        Self { manager, listener }
+    pub(crate) fn new(manager: Manager) -> Self {
+        Self {
+            manager,
+            listener: None,
+            queue: VecDeque::default(),
+        }
+    }
+
+    pub fn play(&mut self, sound: AssetHandle<Audio>, settings: SoundSettings) {
+        self.queue.push_back((sound, settings));
     }
 }
 
